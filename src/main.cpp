85bdfdf996dbbbd803dac1b4b4564993c06314ae
Ignat <ignay@list.ru>
2026-04-09 19:56:03 +0300

    Added file initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "readerIQ.hpp"
 
 int main(){
+    IQ_data data = readfile("../lte_band3.iq", 15.36e6);
+    std::cout << data.NUM_samples << std::endl;
 
-
+    info("Мощность сигнала", powerCalc(data));
 
     return 0;
 }

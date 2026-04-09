dc05f6e94c394dc9ae03d3e15421add2a73f0926
Ignat <ignay@list.ru>
2026-04-09 19:55:16 +0300

    Added power calculation function

--- include/readerIQ.hpp
+++ include/readerIQ.hpp
@@ -9,8 +9,20 @@
 
 struct IQ_data {
     std::vector<comfl32> samples;
-    int NUM_samples = 0;
+    size_t NUM_samples = 0;
     float duration = 0;
 };
 
 IQ_data readfile(std::string filename, float Fs);
+
+
+inline float powerCalc(IQ_data data){
+    float total_power = 0;
+    
+    for(const auto& i : data.samples){
+        total_power += std::norm(i); // I^2 + Q^2
+    }
+
+    return 10.0f*log10((total_power)/data.NUM_samples);
+
+}

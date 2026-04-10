b91c87a2b2114d77900e3530720a26c23d2baed7
Ignat <ignay@list.ru>
2026-04-10 17:39:38 +0300

    Added fft call

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "readerIQ.hpp"
+#include "fft.hpp"
 
 int main(){
     IQ_data data = readfile("../lte_band3.iq", 15.36e6);
-    std::cout << data.NUM_samples << std::endl;
-
     info("Мощность сигнала", powerCalc(data));
+
+    fft_process(data);
 
     return 0;
 }

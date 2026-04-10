dd027bec1964ece54c18c230c9234c422f10cb21
Ignat <ignay@list.ru>
2026-04-10 17:39:17 +0300

    Moved struct on common.hpp

--- include/readerIQ.hpp
+++ include/readerIQ.hpp
@@ -6,13 +6,6 @@
 #include <vector>
 #include "common.hpp"
 
-
-struct IQ_data {
-    std::vector<comfl32> samples;
-    size_t NUM_samples = 0;
-    float duration = 0;
-};
-
 IQ_data readfile(std::string filename, float Fs);
 
 

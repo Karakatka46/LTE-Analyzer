5bf847e1f7fe1982d23e9446edf90cd4c3656b19
Ignat <ignay@list.ru>
2026-04-13 17:52:40 +0300

    Created IFFT function

--- src/fft.cpp
+++ src/fft.cpp
@@ -49,3 +49,30 @@ void fft_forward(std::vector<comfl32>& input){
     //     input[n] /= static_cast<float>(n);
     // }
 }
+
+void ifft(std::vector<comfl32>& input){
+    int n = input.size();
+    bitReverse(input);
+    
+    for(int len = 2; len <= n; len <<= 1){
+        float angle = 2.0f * M_PI / len;
+        comfl32 wlen(cos(angle), sin(angle));
+        
+        for(int i = 0; i < n; i += len){
+            comfl32 w(1,0); // e^(j0) = cos(0) + jsin(0) = 1 + j0 Поворачивающий коэффициент
+            
+            for(int j = 0; j < len/2; ++j){
+                int upper = i + j;          // 0; 1;
+                int lower = i + j + len/2;  // 1; 2;
+                
+                auto u = input[upper];      // (0,0)
+                auto v = input[lower]*w;    // (4,2)*w
+
+                input[upper] = u + v;       // (0,0) + (4,2)*w
+                input[lower] = u - v;       // (0,0) - (4,2)*w
+
+                w *= wlen;                  // w * wlen
+            }
+        }
+    }
+}
